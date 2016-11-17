cba5b2ad595f3f6b0003a5c9c3a8b02d9642ab71
VickyLeu <vickyleu@foxmail.com>
2016-11-17 10:24:32 +0800

    delete unused file

--- index.ios.js
+++ index.ios.js
@@ -9,45 +9,18 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
 
-export default class reactNative extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+//引入外部的js文件
+var LoginView = require('./module/loginView');
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+var QQLogin = React.createClass({
+    render(){
+        return(
+            <LoginView />
+        );
+    }
 });
 
-AppRegistry.registerComponent('reactNative', () => reactNative);
+AppRegistry.registerComponent('QQLogin', () => QQLogin);
